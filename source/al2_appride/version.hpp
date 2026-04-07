cae83d41f15b01e0655a7dbb0b112f2869efd9c8
hebiiro <io.hebiiro@gmail.com>
2026-04-07 18:00:16 +0900

    🔖r2 2026年04月07日

--- source/al2_appride/version.hpp
+++ source/al2_appride/version.hpp
@@ -9,6 +9,6 @@ namespace apn::appride
 		//
 		// コンストラクタです。
 		//
-		version_t() : version_base_t(L"外部アプリ前面表示", L"🐍外部アプリ前面表示🔖", L"r1") {}
+		version_t() : version_base_t(L"外部アプリ前面表示", L"🐍外部アプリ前面表示🔖", L"r2") {}
 	} version;
 }
